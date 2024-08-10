8d3feeeae01c4b6a57eba99668315cadbb47208c
karinanuunes <karina.nuunes@yahoo.com.br>
2024-08-10 10:42:58 -0300

    feat: add menu to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Menu from "@/components/ui/menu";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={montserrat.className}>
+        <main className="flex bg-[#f9f9f9] h-svh">
+          <Menu />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
