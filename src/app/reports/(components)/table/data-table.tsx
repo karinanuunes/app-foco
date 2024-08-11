577b12449016b0fd637c3aad0dbcdae5a0f45ca5
karinanuunes <karina.nuunes@yahoo.com.br>
2024-08-11 18:32:15 -0300

    fix: update empty table message

--- src/app/reports/(components)/table/data-table.tsx
+++ src/app/reports/(components)/table/data-table.tsx
@@ -111,7 +111,7 @@ export function DataTable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  Adicione seus estudos para visualizá-los aqui.
                 </TableCell>
               </TableRow>
             )}
