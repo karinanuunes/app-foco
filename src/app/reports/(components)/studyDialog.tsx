6eb142db9c03e4467e1a41dd7d5990e439e92650
karinanuunes <karina.nuunes@yahoo.com.br>
2024-08-11 18:30:58 -0300

    feat: add new field to enhance data table

--- src/app/reports/(components)/studyDialog.tsx
+++ src/app/reports/(components)/studyDialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -15,10 +17,60 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useState } from "react";
+import * as uuid from "uuid";
 
 const StudyDialog = () => {
-  const handleStudy = () => {
-    alert("Adicionando estudo");
+  const [studyName, setStudyName] = useState("");
+  const [category, setCategory] = useState("");
+  const [timeToComplete, setTimeToComplete] = useState(0);
+  const [completedTime, setCompletedTime] = useState(0);
+  const [priority, setPriority] = useState("");
+  const [status, setStatus] = useState("");
+
+  const handleStudy = (event: React.FormEvent<HTMLFormElement>) => {
+    const temporaryPriority = priority || "Baixa";
+    const temporaryStatus = status || "Pendente";
+
+    const study = {
+      id: uuid.v4(),
+      name: studyName,
+      category,
+      timeToComplete,
+      completedTime,
+      date: new Date().toLocaleDateString(),
+      priority: temporaryPriority,
+      status: temporaryStatus,
+    };
+
+    if (studyName.trim() === "") {
+      event.preventDefault();
+      alert("Nome da matéria é obrigatório!");
+      return;
+    }
+
+    if (category.trim() === "") {
+      event.preventDefault();
+      alert("Categoria é obrigatória!");
+      return;
+    }
+
+    if (timeToComplete == 0) {
+      event.preventDefault();
+      alert("O tempo esperado é obrigatório!");
+      return;
+    }
+
+    const studiesStorage = localStorage.getItem("studies");
+    const studiesParsed = studiesStorage ? JSON.parse(studiesStorage) : [];
+    localStorage.setItem("studies", JSON.stringify([...studiesParsed, study]));
+
+    setStudyName("");
+    setCategory("");
+    setTimeToComplete(0);
+    setCompletedTime(0);
+    setPriority("");
+    setStatus("");
   };
 
   return (
@@ -34,33 +86,51 @@ const StudyDialog = () => {
           <DialogDescription>
             <form className="flex flex-col gap-2 mt-4" onSubmit={handleStudy}>
               <span className="text-base">Nome da matéria</span>
-              <Input id="studyName" className="flex-1" />
+              <Input
+                className="flex-1"
+                value={studyName}
+                onChange={(e) => setStudyName(e.target.value)}
+              />
               <span className="text-base">Categoria</span>
-              <Input id="category" className="flex-1" />
+              <Input
+                className="flex-1"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              />
               <span className="text-base">Tempo esperado</span>
-              <Input id="timeToComplete" className="flex-1" type="number" />
+              <Input
+                className="flex-1"
+                type="number"
+                value={timeToComplete}
+                onChange={(e) => setTimeToComplete(Number(e.target.value))}
+              />
               <span className="text-base">Tempo concluído</span>
-              <Input id="completedTime" className="flex-1" type="number" />
+              <Input
+                className="flex-1"
+                type="number"
+                value={completedTime}
+                onChange={(e) => setCompletedTime(Number(e.target.value))}
+              />
               <span className="text-base">Prioridade</span>
-              <Select>
+              <Select value={priority} onValueChange={setPriority}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Selecione" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="alta">Alta</SelectItem>
-                  <SelectItem value="media">Média</SelectItem>
-                  <SelectItem value="baixa">Baixa</SelectItem>
+                  <SelectItem value="Alta">Alta</SelectItem>
+                  <SelectItem value="Média">Média</SelectItem>
+                  <SelectItem value="Baixa">Baixa</SelectItem>
                 </SelectContent>
               </Select>
               <span className="text-base">Situação</span>
-              <Select>
+              <Select value={status} onValueChange={setStatus}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Selecione" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="pendente">Pendente</SelectItem>
-                  <SelectItem value="concluido">Concluído</SelectItem>
-                  <SelectItem value="incompleto">Incompleto</SelectItem>
+                  <SelectItem value="Pendente">Pendente</SelectItem>
+                  <SelectItem value="Concluído">Concluído</SelectItem>
+                  <SelectItem value="Incompleto">Incompleto</SelectItem>
                 </SelectContent>
               </Select>
               <Button
